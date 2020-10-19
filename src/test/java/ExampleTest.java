69b3b44d2c7ae73eca55e2a7384bc96300d485ff
bdaf <kamilkorenkiewiczpw@gmail.com>
2020-10-19 21:38:34 +0200

    distance example

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,11 +1,21 @@
 import org.junit.jupiter.api.Test;
+import pl.sdk.Point;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ExampleTest {
     @Test
-    void example(){
-        fail();
+    void distanceFromPointShouldBeFive() {
+        //given
+        Point point = new Point(3,4);
+        Point point2 = new Point(6,8);
+        // when
+        double result = point.countDistanceFromXY();
+        double result2 = point2.countDistanceFromXY();
+        //then
+        assertEquals(5.0,result);
+        assertEquals(10.0,result2);
     }
 
 }
