d1ec935c1af4980716bb28489f29b206d6129ed8
bdaf <kamilkorenkiewiczpw@gmail.com>
2020-11-03 23:35:22 +0100

    refactoring in tests

--- src/test/java/pl/sdk/BoardTest.java
+++ src/test/java/pl/sdk/BoardTest.java
@@ -1,18 +1,34 @@
 package pl.sdk;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class BoardTest {
+
+    private Board board;
+    private Creature creature;
+
+    @BeforeEach
+    void init(){
+         board = new Board();
+         creature = new Creature();
+    }
+
     @Test
     void shouldAddCreature(){
-        Board board = new Board();
-        Creature creature = new Creature();
-
         board.add(0,0,creature);
         Creature creatureFromBoard = board.get(0,0);
 
         assertEquals(creature, creatureFromBoard);
+    }
+
+    @Test
+    void shouldReturnNullWhenFieldIsEmpty(){
+        creature = board.get(0,0);
+        assertNull(creature);
     }
 }
