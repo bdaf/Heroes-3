9378119db63defaa412650b0325a19447fd8f795
bdaf <kamilkorenkiewicz@gmail.com>
2020-11-11 17:10:28 +0100

    counter attacking test and implement

--- src/main/java/pl/sdk/Creature.java
+++ src/main/java/pl/sdk/Creature.java
@@ -4,6 +4,7 @@ public class Creature {
 
     private CreatureStatistic stats;
     private int currentHp;
+    private boolean counterAttackInThisTurn;
 
     public Creature() {
         this("DefName",1,1,1,1);
@@ -15,16 +16,22 @@ public class Creature {
     }
 
     public void attack(Creature defender) {
-        this.dealDamage(defender);
-        defender.dealDamage(this);
+        if(isAlive()){
+            int damageToDeal = getDamageToDeal(defender);
+            defender.currentHp-=damageToDeal;
+            if(!defender.counterAttackInThisTurn && defender.isAlive()){
+                int damageToDealInCounterAttack = defender.getDamageToDeal(this);
+                currentHp-=damageToDealInCounterAttack;
+                defender.counterAttackInThisTurn = true;
+            }
+        }
+
     }
 
-    private void dealDamage(Creature defender) {
-        if(isAlive()){
-            int damageToDeal = this.getStats().getAttack() - defender.getStats().getArmor();
-            if(damageToDeal<0) damageToDeal = 0;
-            defender.setCurrentHP(defender.getCurrentHp()-damageToDeal);
-        }
+    private int getDamageToDeal(Creature defender) {
+        int damageToDeal = this.getStats().getAttack() - defender.getStats().getArmor();
+        if(damageToDeal<0) damageToDeal = 0;
+        return damageToDeal;
     }
 
     private boolean isAlive() {
--- src/test/java/pl/sdk/CreatureCounterAttackTest.java
+++ src/test/java/pl/sdk/CreatureCounterAttackTest.java
@@ -20,11 +20,22 @@ public class CreatureCounterAttackTest {
 
     @Test
     void defenderShouldNotCounterAttackWhenHeSupposedToBeDead(){
-        Creature attacker = new Creature("c1",100,NOT_IMPORTANT,NOT_IMPORTANT, NOT_IMPORTANT);
+        Creature attacker = new Creature("c1",100,NOT_IMPORTANT,100, NOT_IMPORTANT);
         Creature defender = new Creature("c2", NOT_IMPORTANT,20,80,NOT_IMPORTANT);
 
         attacker.attack(defender);
 
         assertEquals(100,attacker.getCurrentHp());
+    }
+
+    @Test
+    void defenderShouldCounterAttackOnlyOnes(){
+        Creature attacker = new Creature("c1",5,1,100, NOT_IMPORTANT);
+        Creature defender = new Creature("c2", 10,NOT_IMPORTANT,80,NOT_IMPORTANT);
+
+        attacker.attack(defender);
+        assertEquals(91,attacker.getCurrentHp());
+        attacker.attack(defender);
+        assertEquals(91,attacker.getCurrentHp());
     }
 }
