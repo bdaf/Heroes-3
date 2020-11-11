77290efc4560c76354eef9d08156f8734fc8041a
bdaf <kamilkorenkiewicz@gmail.com>
2020-11-11 16:32:45 +0100

    counter attack is done

---
+++ src/test/java/pl/sdk/CreatureCounterAttackTest.java
@@ -0,0 +1,30 @@
+package pl.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreatureCounterAttackTest {
+
+    private static final int NOT_IMPORTANT = 2;
+
+    @Test
+    void defenderShouldCounterAttack(){
+        Creature attacker = new Creature("c1",NOT_IMPORTANT,2,100, NOT_IMPORTANT);
+        Creature defender = new Creature("c2", 5,NOT_IMPORTANT,NOT_IMPORTANT,NOT_IMPORTANT);
+
+        attacker.attack(defender);
+
+        assertEquals(97,attacker.getCurrentHp());
+    }
+
+    @Test
+    void defenderShouldNotCounterAttackWhenHeSupposedToBeDead(){
+        Creature attacker = new Creature("c1",100,NOT_IMPORTANT,NOT_IMPORTANT, NOT_IMPORTANT);
+        Creature defender = new Creature("c2", NOT_IMPORTANT,20,80,NOT_IMPORTANT);
+
+        attacker.attack(defender);
+
+        assertEquals(100,attacker.getCurrentHp());
+    }
+}
