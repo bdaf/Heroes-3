ff107b9289277ca39f0c5608394d8078411a8e6d
bdaf <kamilkorenkiewicz@gmail.com>
2020-11-11 15:05:19 +0100

    Creature can attack

---
+++ src/test/java/pl/sdk/AttackCreatureTest.java
@@ -0,0 +1,21 @@
+package pl.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AttackCreatureTest {
+
+    public static final int NOT_IMPORTANT = 2;
+
+    @Test
+    void creature2ShouldHaveLost2HPWhenAttackerHas3AttackAndDefenderHas1Armor(){
+        Creature attacker = new Creature("c1",3,NOT_IMPORTANT,10, NOT_IMPORTANT);
+        Creature defender = new Creature("c2", NOT_IMPORTANT,1,10,NOT_IMPORTANT);
+
+        attacker.attack(defender);
+
+        assertEquals(8,defender.getCurrentHP());
+    }
+}
