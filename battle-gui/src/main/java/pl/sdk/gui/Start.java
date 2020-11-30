3bd5b60535cdde5c16d43b3ad29b068669fe468f
bdaf <kamilkorenkiewiczpw@gmail.com>
2020-11-30 16:32:34 +0100

    prepared for GUI

---
+++ battle-gui/src/main/java/pl/sdk/gui/Start.java
@@ -0,0 +1,19 @@
+package pl.sdk.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Scene scene = new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("fxml/battleMap.fxml")));
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
