d594b0276f426d3e1d993630cc7657c683300dba
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-08-27 12:27:23 +0200

    Look of window has been improved

--- battle-gui/src/main/java/pl/sdk/gui/UnitWindow.java
+++ battle-gui/src/main/java/pl/sdk/gui/UnitWindow.java
@@ -1,5 +1,6 @@
 package pl.sdk.gui;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -9,8 +10,11 @@ import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pl.sdk.creatures.Creature;
@@ -23,13 +27,13 @@ public class UnitWindow {
         window.getIcons().add(new Image("jpg/icon.jpg"));
 
         BorderPane pane = new BorderPane();
-        pane.setTop(getLabelOfCreature(aCreature));
-        pane.setLeft(GraphicsOfCreaturesMaker.getGraphicsOf(aCreature.getName(), Creature.Team.LEFT_TEAM, 200, 200));
+        pane.setTop(getLabelOfCreatureName(aCreature));
+        pane.setLeft(GraphicsOfCreaturesMaker.getGraphicsOf(aCreature.getName(), Creature.Team.LEFT_TEAM, 123, 123));
         pane.setRight(getRightVBoxWithStats(aCreature));
-        pane.setBottom(getCloseButton());
+        pane.setBottom(getDescriptionAndCloseButton(aCreature));
 
 
-        Scene scene = new Scene(pane, 450, 250);
+        Scene scene = new Scene(pane, 500, 280);
         scene.getStylesheets().add("fxml/main.css");
 
         window.setScene(scene);
@@ -39,34 +43,57 @@ public class UnitWindow {
         window.setResizable(false);
     }
 
-    private Node getLabelOfCreature(Creature aCreature) {
-        Label label = new Label(aCreature.getName() + "(" + aCreature.getAmount() + ")");
+    private Node getLabelOfCreatureName(Creature aCreature) {
+        Label label = new Label(aCreature.getName() + " (" + aCreature.getAmount() + ")");
         label.setAlignment(Pos.CENTER);
+        label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 24));
         VBox vBox = new VBox(label);
         vBox.setAlignment(Pos.CENTER);
         return vBox;
     }
 
-    private Button getCloseButton() {
+    private VBox getDescriptionAndCloseButton(Creature aCreature) {
         Button closeButton = new Button("Close");
         closeButton.setPrefWidth(200);
         closeButton.setAlignment(Pos.CENTER);
         closeButton.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
             if (e.getButton() == MouseButton.PRIMARY) window.close();
         });
-        return null;
+        Text description = new Text(aCreature.getStats().getDescription());
+        description.setStyle("-fx-font: 12 arial;");
+
+        description.setWrappingWidth(480);
+        VBox vBox = new VBox(5,description,closeButton);
+        vBox.setAlignment(Pos.CENTER);
+        return vBox;
     }
 
     private VBox getRightVBoxWithStats(Creature aCreature) {
-        VBox vBox = new VBox();
+        VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(0,0,20,0));
         String shots = "";
         if (aCreature.getShots() > 0) shots = " - " + aCreature.getShots() + " shots";
-        Label stats = new Label("Health: " + aCreature.getCurrentHp() + "/" + aCreature.getMaxHp() + shots
-                + " |\n Attack: " + aCreature.getStats().getAttack()
-                + " | Damage: " + aCreature.getStats().getDamage().lowerEndpoint() + " - " + aCreature.getStats().getDamage().upperEndpoint()
-                + " |\n Defense: " + aCreature.getStats().getArmor()
+
+        Text healthAndShots = new Text("Health: " + aCreature.getCurrentHp() + "/" + aCreature.getMaxHp() + shots);
+        //healthAndShots.setAlignment(Pos.CENTER);
+
+        Text firstHalfOfStats = new Text("Attack: " + aCreature.getStats().getAttack()
+                + " | Damage: " + aCreature.getStats().getDamage().lowerEndpoint() + " - " + aCreature.getStats().getDamage().upperEndpoint());
+        //firstHalfOfStats.setAlignment(Pos.CENTER);
+
+        Text secondHalfOfStats = new Text("Defense: " + aCreature.getStats().getArmor()
                 + " | Movement: " + aCreature.getStats().getMoveRange());
-        vBox.getChildren().add(stats);
+        //secondHalfOfStats.setAlignment(Pos.CENTER);
+
+        healthAndShots.setStyle("-fx-font: 18 arial;");
+        firstHalfOfStats.setStyle("-fx-font: 18 arial;");
+        secondHalfOfStats.setStyle("-fx-font: 18 arial;");
+
+        vBox.getChildren().add(healthAndShots);
+        vBox.getChildren().add(firstHalfOfStats);
+        vBox.getChildren().add(secondHalfOfStats);
+        vBox.setPrefWidth(370);
         return vBox;
     }
 
