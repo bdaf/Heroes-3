3b584dca583692dc984ee79d91e6d9edb2afd6f3
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-10-01 13:02:59 +0200

    Generally necropolis and castle fractions are done! 1.1.0 Version!

--- battle-model/src/main/java/pl/sdk/GameEngine.java
+++ battle-model/src/main/java/pl/sdk/GameEngine.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Random;
 
 public class GameEngine {
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.1.0";
     public static int BOARD_WIDTH = 20;
     public static int BOARD_HEIGHT = 15;
     public static final String CURRENT_CREATURE_CHANGED = "CURRENT_CREATURE_CHANGED";
